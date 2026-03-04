5ae721b7f5e684b3fdea65eec5f136f449e11ae0
Sreelakshmi R <163765397+imsree2004@users.noreply.github.com>
2026-03-04 14:44:37 +0530

    Delete backend directory

--- backend/nlp/routes/nlpRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { preprocessComplaint } from '../controllers/nlpController.js';
-
-const router = express.Router();
-
-router.post('/preprocess', preprocessComplaint);
-
-export default router;
